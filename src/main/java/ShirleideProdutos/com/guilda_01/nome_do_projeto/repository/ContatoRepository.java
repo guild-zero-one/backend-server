f0dada6ff813779ea777268398eeff17b2df35bd
GustavoLuzz <gustavo.barros@sptech.school>
2025-02-24 10:34:33 -0300

    "feat :tada: Contato repository JPA"

---
+++ src/main/java/ShirleideProdutos/com/guilda_01/nome_do_projeto/repository/ContatoRepository.java
@@ -0,0 +1,7 @@
+package ShirleideProdutos.com.guilda_01.nome_do_projeto.repository;
+
+import ShirleideProdutos.com.guilda_01.nome_do_projeto.model.Contato;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ContatoRepository extends JpaRepository<Contato, Integer> {
+}
