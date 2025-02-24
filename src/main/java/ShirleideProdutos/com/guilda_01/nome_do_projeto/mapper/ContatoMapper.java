1e62af73ce69fb4f1184ebe33e5863cfb0929c02
GustavoLuzz <gustavo.barros@sptech.school>
2025-02-24 11:54:16 -0300

    "feat 🎉 adicionar ContatoMapper para conversão entre DTO e entidade"

---
+++ src/main/java/ShirleideProdutos/com/guilda_01/nome_do_projeto/mapper/ContatoMapper.java
@@ -0,0 +1,26 @@
+package ShirleideProdutos.com.guilda_01.nome_do_projeto.mapper;
+
+import ShirleideProdutos.com.guilda_01.nome_do_projeto.DTO.ClienteDTO;
+import ShirleideProdutos.com.guilda_01.nome_do_projeto.DTO.ContatoDTO;
+import ShirleideProdutos.com.guilda_01.nome_do_projeto.model.Cliente;
+import ShirleideProdutos.com.guilda_01.nome_do_projeto.model.Contato;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ContatoMapper {
+
+    public ContatoDTO toDto(Contato contato) {
+        return new ContatoDTO(
+                contato.getId(), contato.getCelular(), contato.getCriadoEm(), contato.getAtualizadoEm(), contato.getCliente()
+        );
+    }
+
+    public Contato toEntity(ContatoDTO contatoDTO) {
+        return new Contato(contatoDTO.getId(),
+                contatoDTO.getCelular(),
+                contatoDTO.getCriadoEm(),
+                contatoDTO.getAtualizadoEm(),
+                contatoDTO.getCliente());
+    }
+
+}
