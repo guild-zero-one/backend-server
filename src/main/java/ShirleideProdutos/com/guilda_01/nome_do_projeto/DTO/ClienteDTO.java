ed0cde3d696fce5d70aef1e068d974a3b013482c
GustavoLuzz <gustavo.barros@sptech.school>
2025-02-24 10:30:31 -0300

    "feat :tada: ClienteDTO"

---
+++ src/main/java/ShirleideProdutos/com/guilda_01/nome_do_projeto/DTO/ClienteDTO.java
@@ -0,0 +1,33 @@
+package ShirleideProdutos.com.guilda_01.nome_do_projeto.DTO;
+
+import ShirleideProdutos.com.guilda_01.nome_do_projeto.model.Cliente;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import java.time.LocalDate;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class ClienteDTO {
+    private Integer id;
+    private String nome;
+    private LocalDate criadoEm;
+    private LocalDate atualizadoEm;
+    private List<ContatoDTO> contatos;
+
+    public ClienteDTO(Cliente cliente) {
+        this.id = cliente.getId();
+        this.nome = cliente.getNome();
+        this.criadoEm = cliente.getCriadoEm();
+        this.atualizadoEm = cliente.getAtualizadoEm();
+        this.contatos = cliente.getContatos().stream()
+                .map(ContatoDTO::new)
+                .collect(Collectors.toList());
+    }
+
+}
