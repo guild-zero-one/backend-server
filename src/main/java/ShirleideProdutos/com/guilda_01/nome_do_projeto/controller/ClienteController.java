9611f2a3b2c48d842b9662786b04011a8a442dcc
GustavoLuzz <gustavo.barros@sptech.school>
2025-02-24 11:35:53 -0300

    "refactor :recycle: mover lógica do ClienteController para ClienteService"

--- src/main/java/ShirleideProdutos/com/guilda_01/nome_do_projeto/controller/ClienteController.java
+++ src/main/java/ShirleideProdutos/com/guilda_01/nome_do_projeto/controller/ClienteController.java
@@ -1,82 +1,43 @@
 package ShirleideProdutos.com.guilda_01.nome_do_projeto.controller;
 
 import ShirleideProdutos.com.guilda_01.nome_do_projeto.DTO.ClienteDTO;
-import ShirleideProdutos.com.guilda_01.nome_do_projeto.model.Cliente;
-import ShirleideProdutos.com.guilda_01.nome_do_projeto.repository.ClienteRepository;
+import ShirleideProdutos.com.guilda_01.nome_do_projeto.service.ClienteService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import java.time.LocalDate;
+
 import java.util.List;
-import java.util.Optional;
-import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/clientes")
 public class ClienteController {
 
     @Autowired
-    private ClienteRepository clienteRepository;
+    private ClienteService clienteService;
 
     @PostMapping("/cadastrar")
     public ResponseEntity<ClienteDTO> cadastrarCliente(@RequestBody ClienteDTO clienteDTO) {
-        if (clienteDTO.getNome() == null || clienteDTO.getNome().trim().isEmpty()) {
-            return ResponseEntity.badRequest().build();
-        }
-
-        Cliente cliente = new Cliente();
-        cliente.setNome(clienteDTO.getNome());
-
-        Cliente clienteSalvo = clienteRepository.save(cliente);
-
-        return ResponseEntity.ok(new ClienteDTO(clienteSalvo));
+        return ResponseEntity.ok(clienteService.cadastrarCliente(clienteDTO));
     }
 
-
     @GetMapping
-    public List<ClienteDTO> listar() {
-        return clienteRepository.findAll()
-                .stream()
-                .map(ClienteDTO::new)
-                .collect(Collectors.toList());
+    public List<ClienteDTO> listarClientes() {
+        return clienteService.listarClientes();
     }
 
     @GetMapping("/{id}")
     public ResponseEntity<ClienteDTO> buscarClientePorId(@PathVariable Integer id) {
-        return clienteRepository.findById(id)
-                .map(cliente -> ResponseEntity.ok(new ClienteDTO(cliente)))
-                .orElse(ResponseEntity.notFound().build());
+        return ResponseEntity.ok(clienteService.buscarClientePorId(id));
     }
 
     @PatchMapping("/{id}")
-    @ResponseStatus(HttpStatus.NO_CONTENT)
-    public ResponseEntity<ClienteDTO> atualizarCliente(@PathVariable Integer id, @RequestBody ClienteDTO cliente) {
-        Optional<Cliente> clienteExistente = clienteRepository.findById(id);
-
-        if (clienteExistente.isPresent()) {
-            Cliente clienteAtualizado = clienteExistente.get();
-            clienteAtualizado.setNome(cliente.getNome());
-            clienteAtualizado.setAtualizadoEm(LocalDate.now());
-
-            clienteRepository.save(clienteAtualizado);
-
-            return ResponseEntity.ok(new ClienteDTO(clienteAtualizado));
-        }
-        return ResponseEntity.notFound().build();
+    public ResponseEntity<ClienteDTO> atualizarCliente(@PathVariable Integer id, @RequestBody ClienteDTO clienteDTO) {
+        return ResponseEntity.ok(clienteService.atualizarCliente(id, clienteDTO));
     }
 
     @DeleteMapping("/{id}")
-    @ResponseStatus(HttpStatus.NO_CONTENT)
-    public String deletarCliente(@PathVariable Integer id) {
-
-        if(clienteRepository.existsById(id)){
-            clienteRepository.deleteById(id);
-            return "Cliente removido com sucesso! ";
-        }
-
-        return "Cliente não encontrado!";
-
+    public ResponseEntity<Void> deletarCliente(@PathVariable Integer id) {
+        clienteService.deletarCliente(id);
+        return ResponseEntity.noContent().build();
     }
-
 }
