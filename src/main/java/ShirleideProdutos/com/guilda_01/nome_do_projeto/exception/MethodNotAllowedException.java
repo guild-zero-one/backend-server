fab71312a5219bf7522fd052765d49ffeeceb9dc
enricoferrer <enrico.santana@sptech.school>
2025-03-06 16:08:24 -0300

    feat :tada: Exception: Not Allowed (405)

---
+++ src/main/java/ShirleideProdutos/com/guilda_01/nome_do_projeto/exception/MethodNotAllowedException.java
@@ -0,0 +1,7 @@
+package ShirleideProdutos.com.guilda_01.nome_do_projeto.exception;
+
+public class MethodNotAllowedException extends RuntimeException{
+    public MethodNotAllowedException(String mensagem){
+        super(mensagem);
+    }
+}
