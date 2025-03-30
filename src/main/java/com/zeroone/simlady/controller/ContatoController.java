3cca4a0032eb48eba177edc56a1026257d0443bf
GustavoLuzz <gustavo.barros@sptech.school>
2025-03-30 16:01:26 -0300

    refactor :hammer: refatoração completa da camada de serviço e controle da rota de contatos.

--- src/main/java/com/zeroone/simlady/controller/ContatoController.java
+++ src/main/java/com/zeroone/simlady/controller/ContatoController.java
@@ -1,8 +1,10 @@
 package com.zeroone.simlady.controller;
 
-import com.zeroone.simlady.dto.ContatoDTO;
+import com.zeroone.simlady.dto.contato.ContatoRequestDto;
+import com.zeroone.simlady.dto.contato.ContatoResponseDto;
 import com.zeroone.simlady.entity.Contato;
 import com.zeroone.simlady.service.ContatoService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -13,36 +15,47 @@ import java.util.Set;
 @RequestMapping("/contatos")
 public class ContatoController {
 
-
-    @Autowired
     ContatoService contatoService;
 
+    public ContatoController(ContatoService contatoService) {
+        this.contatoService = contatoService;
+    }
+
     @PostMapping("/{clienteId}")
-    public ResponseEntity<ContatoDTO> adicionarContato(
-            @PathVariable Integer clienteId,
-            @RequestBody Contato contato) {
+    public ResponseEntity<ContatoResponseDto> adicionar(@PathVariable Integer clienteId,
+                                                               @Valid @RequestBody ContatoRequestDto dto) {
         return ResponseEntity.status(201)
-                .body(contatoService.adicionarContato(clienteId, contato));
+                .body(contatoService
+                        .adicionar(clienteId, dto));
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<ContatoDTO> buscar(@PathVariable Integer id) {
-        return ResponseEntity.ok(contatoService.buscarContatoPorId(id));
+    public ResponseEntity<ContatoResponseDto> buscar(@PathVariable Integer id) {
+
+        return ResponseEntity
+                .ok(contatoService
+                        .buscarPorId(id));
     }
 
     @GetMapping("/cliente/{id}")
-    public ResponseEntity<Set<ContatoDTO>> buscarContatosPorCliente(@PathVariable Integer id) {
-        return ResponseEntity.ok(contatoService.buscarContatosPorCliente(id));
+    public ResponseEntity<Set<ContatoResponseDto>> buscarPorCliente(@PathVariable Integer id) {
+
+        return ResponseEntity
+                .ok(contatoService
+                        .buscarPorCliente(id));
     }
 
     @PatchMapping("/{id}")
-    public ResponseEntity<ContatoDTO> atualizarContato(@PathVariable Integer id, @RequestBody  Contato contato) {
-        return ResponseEntity.ok(contatoService.atualizarContato(id, contato));
+    public ResponseEntity<ContatoResponseDto> atualizar(@PathVariable Integer id,
+                                                               @Valid @RequestBody  ContatoRequestDto dto) {
+        return ResponseEntity
+                .ok(contatoService
+                                .atualizar(id, dto));
     }
 
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> deletarContato (@PathVariable Integer id) {
-        contatoService.deletarContato(id);
+        contatoService.deletar(id);
         return ResponseEntity.noContent().build();
     }
 
--- src/main/java/com/zeroone/simlady/repository/ContatoRepository.java
+++ src/main/java/com/zeroone/simlady/repository/ContatoRepository.java
@@ -1,11 +1,17 @@
 package com.zeroone.simlady.repository;
 
 import com.zeroone.simlady.entity.Contato;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 
 import java.util.Optional;
 
 public interface ContatoRepository extends JpaRepository<Contato, Integer> {
-     Optional<Contato> findByCelular(String celular);
+
+     boolean existsByCelular(@NotBlank(message = "Celular não deve ser vazio ou nulo.") @Pattern(regexp = "^\\d{11}$", message = "Número de celular inválido. Deve conter 11 dígitos.") String celular);
+
+     boolean existsByCelularAndIdNot(@Size(min = 11, max = 11, message = "O número de celular deve ter exatamente 11 dígitos") @Pattern(regexp = "\\d{11}", message = "O número de celular deve conter apenas números") String celular, Integer id);
 }
