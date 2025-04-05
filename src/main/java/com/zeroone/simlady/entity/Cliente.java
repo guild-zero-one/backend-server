848b4e4be613e8c697a1df11f6e9f352f0ed293f
GustavoLuzz <gustavo.barros@sptech.school>
2025-04-05 14:58:15 -0300

    refactor :hammer: removendo validações de entidades para evitar funil.

--- src/main/java/com/zeroone/simlady/entity/Cliente.java
+++ src/main/java/com/zeroone/simlady/entity/Cliente.java
@@ -1,15 +1,11 @@
 package com.zeroone.simlady.entity;
 
 import jakarta.persistence.*;
-import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
-import org.hibernate.validator.constraints.br.CPF;
+
 import java.time.LocalDate;
 import java.util.HashSet;
 import java.util.Set;
@@ -24,29 +20,13 @@ public class Cliente {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
-    @NotBlank(message = "Nome não deve ser vazio")
-    @Size(min = 3, max = 30, message = "Nome deve ter entre 3 e 30 caracteres")
     private String nome;
-
-    @Size(min = 3, max = 30, message = "Sobrenome deve ter entre 3 e 30 caracteres")
     private String sobrenome;
-
-    @Size(min = 2, max = 15, message = "Apelido deve ter entre 2 e 15 caracteres")
     private String apelido;
-
-    @CPF(message = "CPF inválido")
-    @NotBlank(message = "CPF não pode ser vazio")
     private String cpf;
-
-    @Email(message = "Email inválido")
-    @NotBlank(message = "Email não pode ser vazio")
     private String email;
-
-    @NotBlank(message = "Senha não pode ser vazia")
-    @Size(min = 8, max = 30, message = "Senha deve ter entre 8 e 30 caracteres")
     private String senha;
 
-    @NotNull
     private Boolean ativo = true;
 
     @CreationTimestamp
@@ -62,4 +42,3 @@ public class Cliente {
         contatos.add(contato);
     }
 }
-
