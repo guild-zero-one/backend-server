027e9003636c81e6525b538d1420480cbb01ba79
GustavoLuzz <161068517+GustavoLuzz@users.noreply.github.com>
2025-04-27 12:58:08 -0300

    "feat :tada: liberando todas rotas em ambiente de desenvolvimento"

--- src/main/java/com/zeroone/simlady/config/security/SecurityConfiguracao.java
+++ src/main/java/com/zeroone/simlady/config/security/SecurityConfiguracao.java
@@ -54,7 +54,8 @@ public class SecurityConfiguracao {
             new AntPathRequestMatcher("/h2-console/**/**"),
             new AntPathRequestMatcher("/error/**"),
             new AntPathRequestMatcher("/usuarios/**"),
-            new AntPathRequestMatcher("/produtos/**"),
+            new AntPathRequestMatcher("/produtos/**")
+            new AntPathRequestMatcher("/**")
     };
 
     @Bean
