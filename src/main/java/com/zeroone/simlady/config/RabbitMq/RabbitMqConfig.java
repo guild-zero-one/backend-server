eb8461545ce10abf87b7c534c21093a5f96ddbf4
GustavoLuzz <gustavo.barros@sptech.school>
2025-04-28 02:37:42 -0300

    "fix :hammer: Modificações para jar funcionar corretamente no Docker Compose "

--- src/main/java/com/zeroone/simlady/config/RabbitMq/RabbitMqConfig.java
+++ src/main/java/com/zeroone/simlady/config/RabbitMq/RabbitMqConfig.java
@@ -2,6 +2,10 @@ package com.zeroone.simlady.config.RabbitMq;
 
 import org.springframework.amqp.core.Declarable;
 import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.core.Exchange;
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.rabbit.core.RabbitAdmin;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -10,15 +14,30 @@ import org.springframework.context.annotation.Configuration;
 public class RabbitMqConfig {
 
     public static final String ORDER_CREATED_QUEUE = "simlady-order-created";
+    public static final String EXCHANGE_NAME = "simlady-exchange";
+
+    @Bean
+    public Queue orderCreatedQueue() {
+        return new Queue(ORDER_CREATED_QUEUE, true);
+    }
+
+    @Bean
+    public Exchange orderCreatedExchange() {
+        return new org.springframework.amqp.core.DirectExchange(EXCHANGE_NAME, true, false);
+    }
 
     @Bean
     public Jackson2JsonMessageConverter jackson2JsonMessageConverter() {
         return new Jackson2JsonMessageConverter();
     }
 
-    @Bean
-    public Declarable orderCreatedQueue() {
-        return new Queue(ORDER_CREATED_QUEUE, true);
-    }
 
+    @Bean
+    public RabbitAdmin rabbitAdmin(RabbitTemplate rabbitTemplate) {
+        RabbitAdmin rabbitAdmin = new RabbitAdmin(rabbitTemplate);
+        rabbitAdmin.declareQueue(orderCreatedQueue());
+        rabbitAdmin.declareExchange(orderCreatedExchange());
+        System.out.println("Fila e Exchange criados com sucesso no RabbitMQ!");
+        return rabbitAdmin;
+    }
 }
