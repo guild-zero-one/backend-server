68a477c71a5e01f721b0fb62de0c25f8eead42a5
enricoferrer <enrico.santana@sptech.school>
2025-05-07 18:36:29 -0300

    refactor :hammer: refatorizando FornecedorRequestDto

--- src/main/java/com/zeroone/simlady/dto/fornecedor/FornecedorRequestDto.java
+++ src/main/java/com/zeroone/simlady/dto/fornecedor/FornecedorRequestDto.java
@@ -12,10 +12,14 @@ public class FornecedorRequestDto {
     @Schema(description = "Nome do fornecedor/marca", example = "Boticário")
     private String nome;
 
+    @NotBlank(message = "Descrição não pode ser vazia.")
+    @Schema(description = "Descrição do fornecedor/marca", example = "Produtos de beleza e cuidados pessoais")
+    private String descricao;
+
+    @Schema(description = "URL da imagem do fornecedor/marca", example = "https://example.com/imagem.jpg")
+    private String imagemUrl;
+
     @CNPJ(message = "CNPJ inválido.")
     @Schema(description = "CNPJ", example = "10.164.222/0001-01")
-    @NotBlank(message = "Descrição não deve ser vazio")
-    private String descricao;
-    private String imagemUrl;
     private String cnpj;
 }
