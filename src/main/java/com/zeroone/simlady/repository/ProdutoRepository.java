5b7442d612b25b481794974abda84d9207b16e84
joaoGlima09 <joao.glima@sptech.school>
2025-08-15 00:36:52 -0300

    refactor :wrench: removendo scopedvalue

--- src/main/java/com/zeroone/simlady/repository/ProdutoRepository.java
+++ src/main/java/com/zeroone/simlady/repository/ProdutoRepository.java
@@ -3,7 +3,6 @@ package com.zeroone.simlady.repository;
 import com.zeroone.simlady.entity.Produto;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.lang.ScopedValue;
 import java.util.List;
 
 public interface ProdutoRepository extends JpaRepository<Produto, Integer> {
