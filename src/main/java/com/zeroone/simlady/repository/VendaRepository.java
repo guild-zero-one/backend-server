6f3eb2ae31453d853222a75bb1191e4af9abc4ac
enricoferrer <enrico.santana@sptech.school>
2026-04-15 09:55:09 -0300

    "feat:venda atualizado"

--- src/main/java/com/zeroone/simlady/repository/VendaRepository.java
+++ src/main/java/com/zeroone/simlady/repository/VendaRepository.java
@@ -6,7 +6,6 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
-import java.awt.print.Pageable;
 import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.util.List;
